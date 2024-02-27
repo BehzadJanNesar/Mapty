1b962b96dac5ef80f4340a1d04c11a7cc81d53cd
BehzadJanNesar <behzadjannesar.2001@gmail.com>
2024-02-27 16:40:41 +0330

    Changed code to OOP style

--- script.js
+++ script.js
@@ -10,59 +10,75 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
-
 let map, mapEvents;
-if (navigator.geolocation)
-   navigator.geolocation.getCurrentPosition(
-      function (position) {
-         const MyPosition = position.coords;
-         const { latitude, longitude } = MyPosition;
-         console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
 
-         map = L.map("map").setView([latitude, longitude], 13);
+class App {
+   constructor() {
+      this._getposition();
+   }
 
-         L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-         }).addTo(map);
-
-         L.marker([latitude, longitude])
-            .addTo(map)
-            .bindPopup("This is your  Location...", {
-               autoClose: false,
-               closeOnClick: false,
-               className: "running-popup",
-            })
-            .openPopup();
-
-         map.on("click", function (mapE) {
-            mapEvents = mapE;
-            form.classList.remove("hidden");
-            inputDistance.focus();
+   _getposition() {
+      if (navigator.geolocation)
+         navigator.geolocation.getCurrentPosition(this._loadMap, function () {
+            alert("could not get your position...");
          });
-      },
-      function () {
-         alert("could not get your position...");
-      }
-   );
-// if (inputType)
-form.addEventListener("submit", function (e) {
-   e.preventDefault();
-   inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = "";
-   const targetPosition = mapEvents.latlng;
-   const { lat, lng } = targetPosition;
-   L.marker([lat, lng])
-      .addTo(map)
-      .bindPopup("Walker", {
-         maxWidth: 250,
-         minWidth: 110,
-         autoClose: false,
-         closeOnClick: false,
-         className: "cycling-popup",
-      })
-      .openPopup();
-});
+   }
 
-inputType.addEventListener("change", () => {
-   inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
-   inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
-});
+   _loadMap(position) {
+      const MyPosition = position.coords;
+      const { latitude, longitude } = MyPosition;
+      console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+
+      map = L.map("map").setView([latitude, longitude], 13);
+
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker([latitude, longitude])
+         .addTo(map)
+         .bindPopup("This is your  Location...", {
+            autoClose: false,
+            closeOnClick: false,
+            className: "running-popup",
+         })
+         .openPopup();
+
+      map.on("click", function (mapE) {
+         mapEvents = mapE;
+         form.classList.remove("hidden");
+         inputDistance.focus();
+      });
+   }
+
+   _showForm() {}
+
+   _toggleElevationField() {}
+
+   _newWorkout() {}
+}
+
+const app = new App();
+
+// if (inputType)
+// form.addEventListener("submit", function (e) {
+//    e.preventDefault();
+//    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = "";
+//    const targetPosition = mapEvents.latlng;
+//    const { lat, lng } = targetPosition;
+//    L.marker([lat, lng])
+//       .addTo(map)
+//       .bindPopup("Walker", {
+//          maxWidth: 250,
+//          minWidth: 110,
+//          autoClose: false,
+//          closeOnClick: false,
+//          className: "cycling-popup",
+//       })
+//       .openPopup();
+// });
+
+// inputType.addEventListener("change", () => {
+//    inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+//    inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+// });
