6673875979e839cbfac0eaf9ce20a50ad14aefcd
BehzadJanNesar <behzadjannesar.2001@gmail.com>
2024-02-29 00:02:48 +0330

    created workout class & methods

--- script.js
+++ script.js
@@ -10,33 +10,81 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
-let map, mapEvents;
 
-class App {
-   constructor() {
-      this._getposition();
+class Workout {
+   date = new Date();
+   id = (Date.now() + "").slice(-10);
+   constructor(coords, distance, duration) {
+      this.coords = coords;
+      this.distance = distance;
+      this.duration = duration;
+   }
+}
+
+class Running extends Workout {
+   constructor(coords, distance, duration, cadence) {
+      super(coords, distance, duration);
+      this.cadence = cadence;
+      this.calcPace();
    }
 
+   calcPace() {
+      //calc as min/km
+      this.pace = this.duration / this.distance;
+      return this.pace;
+   }
+}
+class Cycling extends Workout {
+   constructor(coords, distance, duration, elevationGain) {
+      super(coords, distance, duration);
+      this.elevationGain = elevationGain;
+      this.calcSpeed();
+   }
+
+   calcSpeed() {
+      // calc as km/hour
+      this.speed = this.distance / (this.duration / 60);
+      return this.speed;
+   }
+}
+
+const run1 = new Running();
+const cycling1 = new Cycling();
+console.log(run1, cycling1);
+
+/////////////////////////////////////////////////////////////////////////
+class App {
+   #map;
+   #mapEvents;
+   // constructor section
+   constructor() {
+      this._getposition();
+      form.addEventListener("submit", this._newWorkout.bind(this));
+      inputType.addEventListener("change", this._toggleElevationField);
+   }
+
+   // start set methods
+
+   // get map position methods
    _getposition() {
       if (navigator.geolocation)
-         navigator.geolocation.getCurrentPosition(this._loadMap, function () {
+         navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function () {
             alert("could not get your position...");
          });
    }
-
+   // load map method
    _loadMap(position) {
       const MyPosition = position.coords;
       const { latitude, longitude } = MyPosition;
-      console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
-
-      map = L.map("map").setView([latitude, longitude], 13);
+      this.#map = L.map("map").setView([latitude, longitude], 13);
 
       L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+         attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(this.#map);
 
       L.marker([latitude, longitude])
-         .addTo(map)
+         .addTo(this.#map)
          .bindPopup("This is your  Location...", {
             autoClose: false,
             closeOnClick: false,
@@ -44,41 +92,64 @@ class App {
          })
          .openPopup();
 
-      map.on("click", function (mapE) {
-         mapEvents = mapE;
-         form.classList.remove("hidden");
-         inputDistance.focus();
-      });
+      this.#map.on("click", this._showForm.bind(this));
+   }
+   // show form box method
+   _showForm(mapE) {
+      this.#mapEvents = mapE;
+      form.classList.remove("hidden");
+      inputDistance.focus();
    }
 
-   _showForm() {}
+   // toggle Elevation input value field
+   _toggleElevationField() {
+      inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+      inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+   }
+   // add new workout method
+   _newWorkout(e) {
+      const validateInput = (...inputes) => {
+         console.log(inputes);
+      };
+      validateInput("ali", "behzad", ":reza");
 
-   _toggleElevationField() {}
+      e.preventDefault();
 
-   _newWorkout() {}
+      // get data from form
+      const type = inputType.value,
+         distance = +inputDistance.value,
+         duration = +inputDuration.value;
+
+      // check validation of data
+
+      // If workout is Running, create running object
+      if (type === "running") {
+         const cadence = +inputCadence.value;
+         if (!Number.isFinite(cadence) || !Number.isFinite(distance) || !Number.isFinite(duration))
+            return alert("Inputs have to be positive number...");
+      }
+
+      // If workout is Cycling, create running cycling
+      if (type === "cycling") {
+         const elevation = +inputElevation.value;
+      }
+
+      // add new object to array
+
+      inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = "";
+      const targetPosition = this.#mapEvents.latlng;
+      const { lat, lng } = targetPosition;
+      L.marker([lat, lng])
+         .addTo(this.#map)
+         .bindPopup("Walker", {
+            maxWidth: 250,
+            minWidth: 110,
+            autoClose: false,
+            closeOnClick: false,
+            className: "cycling-popup",
+         })
+         .openPopup();
+   }
 }
 
 const app = new App();
-
-// if (inputType)
-// form.addEventListener("submit", function (e) {
-//    e.preventDefault();
-//    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = "";
-//    const targetPosition = mapEvents.latlng;
-//    const { lat, lng } = targetPosition;
-//    L.marker([lat, lng])
-//       .addTo(map)
-//       .bindPopup("Walker", {
-//          maxWidth: 250,
-//          minWidth: 110,
-//          autoClose: false,
-//          closeOnClick: false,
-//          className: "cycling-popup",
-//       })
-//       .openPopup();
-// });
-
-// inputType.addEventListener("change", () => {
-//    inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
-//    inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
-// });
